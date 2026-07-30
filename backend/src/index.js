12f179b7dbdaa449aff96aa56a99d9cfe0511aa1
Fahim <rkonowheren@gmail.com>
2026-07-30 12:58:51 +0530

    Add models use middleware , cors config and db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,23 @@
+import "dotenv/config"; 
 import express from "express";
-import "dotenv/config";
+import cors from "cors";
+import {clerkMiddleware} from '@clerk/express'
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
 
-//VP6P1mBdIGLY3J6o
+const app = express();
+const port = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-const app = express()
-const port = process.env.PORT
-app.listen(port, () => console.log(`Server is Running ${port}`));
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL, credentials:true}))
+app.use(clerkMiddleware())
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ok: true})
+});
+
+app.listen (port, () => {
+    connectDB();
+    console.log(`server is running ${port}`)
+})
