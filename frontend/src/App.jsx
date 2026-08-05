59ce118cc7929ab9c7ea05bb901c8478a04bd670
Fahim <rkonowheren@gmail.com>
2026-08-05 17:10:09 +0530

    feat: simplify PageLoader component by removing animations and unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { WallpaperProvider } from "./context/WallpaperContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { Navigate, Route, Routes } from "react-router";
@@ -7,40 +6,26 @@ import AuthPage from "./pages/AuthPage";
 import { useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 
+
+
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
-  const [timeLoading, setTimeLoading] = useState(true);
 
-  useEffect(() => {
-    // Keeps loader mounted for exactly 5 seconds
-    const timer = setTimeout(() => {
-      setTimeLoading(false);
-    }, 5000);
 
-    return () => clearTimeout(timer);
-  }, []);
-
-  const showLoader = !isLoaded || timeLoading;
+  if (!isLoaded) return <PageLoader />;
 
   return (
-    <>
-      {showLoader && <PageLoader />}
-      
-      <ThemeProvider>
-        <WallpaperProvider>
-          <Routes>
-            <Route 
-              path="/" 
-              element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} 
-            />
-            <Route 
-              path="/auth" 
-              element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} 
-            />
-          </Routes>
-        </WallpaperProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
