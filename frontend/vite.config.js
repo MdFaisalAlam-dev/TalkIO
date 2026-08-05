78abd259f2bd0577505c0476f6b57b155618eddc
Fahim <rkonowheren@gmail.com>
2026-08-05 17:36:36 +0530

    feat: add build configuration for code splitting in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,4 +10,19 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-})
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20000,
+          groups: [
+            {
+              name: 'vendor',
+              test: /node_modules/, // Splits massive libraries like HeroUI into smaller pieces
+            },
+          ],
+        },
+      },
+    },
+  },
+});
