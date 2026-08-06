2dad992b6acf25d4f62f9c52d231e89336c99129
Fahim <rkonowheren@gmail.com>
2026-08-06 10:58:10 +0530

    feat: update package dependencies and simplify Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,10 @@
-import { defineConfig } from 'vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import babel from '@rolldown/plugin-babel';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
   ],
-  build: {
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          minSize: 20000,
-          groups: [
-            {
-              name: 'vendor',
-              test: /node_modules/, // Splits massive libraries like HeroUI into smaller pieces
-            },
-          ],
-        },
-      },
-    },
-  },
 });
